d70154224ed22bd0ab8312a35bcacf9655089eb7
Rezd22 <prasanjayagalang22@gmail.com>
2022-06-19 12:13:52 +0700

    first commit

---
+++ FastToken.js
@@ -0,0 +1,22 @@
+$('.like-btn').on('click', function() {
+    $(this).toggleClass('is-active');
+ });
+
+ $('.minus-btn').on('click', function(e) {
+    e.preventDefault();
+    var $this = $(this);
+    var $input = $this.closest('div').find('input');
+    var value = parseInt($input.val());
+  $input.val(value);
+ 
+});
+ 
+$('.plus-btn').on('click', function(e) {
+    e.preventDefault();
+    var $this = $(this);
+    var $input = $this.closest('div').find('input');
+    var value = parseInt($input.val());
+ 
+ 
+    $input.val(value);
+});
